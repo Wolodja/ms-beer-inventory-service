ee0bd3ce5ccd5dcbf718a9669b88581780785d99
Wolodja <wolodjawaskovych@i.ua>
2021-02-26 17:58:21 +0100

    Handling Allocation Request. Closes #14

--- src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
@@ -23,19 +23,26 @@ public class BeerOrderLineDto {
     @JsonProperty("version")
     private Integer version = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @JsonProperty("createdDate")
     private OffsetDateTime createdDate = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @JsonProperty("lastModifiedDate")
     private OffsetDateTime lastModifiedDate = null;
 
     private String upc;
+
     private String beerName;
+
     private String beerStyle;
+
     private UUID beerId;
+
     private Integer orderQuantity = 0;
+
     private Integer quantityAllocated;
+
     private BigDecimal price;
+
 }
